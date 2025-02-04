81be3bc877c87d05f79334054c4aad596dd097c8
Ryan S. Castro Felix <rycastro@viva.com.do>
2025-02-04 07:35:39 -0400

    feat: agrega constantes

---
+++ constants/errors.ts
@@ -0,0 +1,4 @@
+export const CODE_MESSAGES: any = {
+  ECONNABORTED:
+    'El tiempo de espera fue demasiado. Revisa tu conexión a internet o intentalo mas tarde.',
+};
---
+++ constants/styles.ts
@@ -0,0 +1,15 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  boxShadow: {
+    shadowColor: '#D9D9D9',
+    shadowOffset: {
+      width: 0,
+      height: 6,
+    },
+    shadowOpacity: 0.37,
+    shadowRadius: 7.49,
+
+    elevation: 12,
+  },
+});
